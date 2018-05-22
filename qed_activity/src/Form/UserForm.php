7a3dbc05ae43dae7f78f9eabb4d08b9ab52a268f
asmita <asmita.wagh@qed42.com>
2018-05-22 14:27:36 +0530

    changes added to userform

--- qed_activity/src/Form/UserForm.php
+++ qed_activity/src/Form/UserForm.php
@@ -2,6 +2,7 @@
 namespace Drupal\qed_activity\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Database\Connection;
 
 class UserForm extends FormBase {
 
@@ -37,12 +38,9 @@ public function buildForm(array $form, FormStateInterface $form_state) {
 }
 
 public function submitForm(array &$form, FormStateInterface $form_state) {
-
-    $connection = \Drupal::database();
-    $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id());
-    $uid = $user->get('uid')->value;
-
-    $query = $connection->insert('user_information')->fields([
+     $connection = \Drupal::database();
+     $uid   = $this->currentUser()->id();
+     $query = $connection->insert('user_information')->fields([
     'uid' => $uid,
     'message' => $form_state->getValue('message'),
     'name' => $form_state->getValue('name'),
